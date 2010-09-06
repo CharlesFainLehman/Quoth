ef53f49b9e11a67654b8929bd9cfabb4323cc0fc
Charles Lehman <greenknight04@gmail.com>
2010-09-06 15:30:41 -0400

    added QuothSet, which defines a markov chain

---
+++ QuothSet.rb
@@ -0,0 +1,61 @@
+class QuothSet
+	@corpus
+	
+	def initialize(text)
+		@corpus = {}
+		set = text.split
+		set.each_index do |i|
+			if i < set.length - 1 then #only to the second to last word
+				if @corpus.has_key? set[i] then #if the key already exists
+					@corpus[set[i]] << set[i+1] 
+				else
+					@corpus[set[i]] = [set[i+1]]
+				end
+			end
+		end
+	end
+	
+	def get(startWord, length)
+		ret = startWord + " "
+		word = startWord
+		count = 0
+		
+		while @corpus.has_key? word and count < length
+			newWord = @corpus[word][rand(@corpus[word].length)]
+			ret << newWord + " " #get the value at a random position of the array returned as the value of the key word
+			word = newWord
+			count += 1
+		end
+		
+		ret
+	end
+	
+	def addSet(key, value)
+		@corpus.has_key? key ? @corpus[key] << value : @corpus[key] = [value]
+	end
+	
+	def addText(text)
+		set = text.split
+		set.each_index do |i|
+			if i < set.length - 1 then addSet(set[i],set[i+1]) end
+		end
+	end
+	
+	def to_s
+		ret = "{"
+		@corpus.each_pair do |key, value|
+			ret << "#{key.to_s} => ["
+			for word in value do ret << word + "," end
+			ret.chop!
+			ret << "], "
+		end
+		ret.chop!
+		ret.chop!
+		ret << "}"
+		return ret
+	end
+end
+
+test = QuothSet.new("once upon a midnight dreary while I pondered weak and weary over many a quaint and curious volume of forgotten lore while I nodded nearly napping suddenly there came a tapping as of something gently wrapping wrapping at my chamber door tis some vistor I muttered tapping at my chamber door this it is and nothing more")
+puts test
+puts test.get("once",2)
