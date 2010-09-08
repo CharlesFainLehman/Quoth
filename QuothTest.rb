b33cb148f48b4604ece2d789896deb824b7e4778
Charles Lehman <greenknight04@gmail.com>
2010-09-08 17:43:23 -0400

    added The Raven, yaml writing in the example

--- QuothTest.rb
+++ QuothTest.rb
@@ -1,8 +1,13 @@
 require "Quoth.rb"
 
-f = File.new("A Christmas Carol.txt")
-test = Quoth.new(f)
-f.close
-puts test.get(ARGV[0],ARGV[1].to_i)
-test.to_yaml("./output.yaml")
+f1 = File.new("A Christmas Carol.txt")
+quoth1 = Quoth.new(f1.read)
+f1.close
+
+f2 = File.new("The Raven.txt")
+quoth2 = Quoth.new(f2.read)
+f2.close
+
+puts quoth1.get(ARGV[0],ARGV[1].to_i)
+quoth2.to_yaml("./output.yaml")
 #puts (ARGV[0].split.length > 3 and ARGV[1].to_i > 0) ? test.get(ARGV[0],ARGV[1].to_i) : "improperly formatted arguments!"
