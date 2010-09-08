a515652f3300eb147921ef600cda1c25f486440f
Charles Fain Lehman <greenknight04@gmail.com>
2010-09-08 12:01:26 -0400

    added argument error handling sort of

--- DoubleWordQuothSet.rb
+++ DoubleWordQuothSet.rb
@@ -60,4 +60,5 @@ test = QuothSet.new(f.read)
 f.close
 #puts test
 puts test.get(ARGV[0],ARGV[1].to_i)
+#puts (ARGV[0].split.length > 3 and ARGV[1].to_i > 0) ? test.get(ARGV[0],ARGV[1].to_i) : "improperly formatted arguments!"
 #test.write
