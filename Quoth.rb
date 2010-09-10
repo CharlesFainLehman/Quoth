7e430e9f3d978fff2c7fe937efb1cd2ffd497cad
Charles Fain Lehman <greenknight04@gmail.com>
2010-09-10 11:59:06 -0400

    new doc + moar encapsulation

--- Quoth.rb
+++ Quoth.rb
@@ -7,7 +7,7 @@ require "yaml"
 #the corpus is constructed from any large block of text- words are organized by sets of two and then have the word following them associated with them. The corpus can also be constructed from a previously generated .yaml file created by the to_yaml() method.
 #When accessing the corpus, the get method looks at a string of two words (for example "I am"), splits it and then uses the resultant array as the key. The value is tacked on to the end of the first string, and the second word in the string ("am") is used alongside the returned value to retrieve the next word.
 class Quoth
-	#the main corpus- contains sets of words and their associated next words.
+	#The main corpus- contains sets of words and their associated next words.
 	attr_reader :corpus
 	
 	#Accepts a .txt, a .yaml or just straight text
@@ -16,7 +16,7 @@ class Quoth
 		add text
 	end
 	
-	#returns a sentence beginning with startWord and of length length + 1
+	#Returns a sentence beginning with startWord and of length length + 1.
 	def get(startWord, length)
 		ret = startWord + " "
 		word = startWord.split
@@ -32,24 +32,40 @@ class Quoth
 		ret
 	end
 	
-	#a general method for adding a .txt, a .yaml, straight text or a [key,key] => value to the corpus.
-	#If adding something besides [key,key] => value, just pass an argument for key1
+	#A general method for adding a .txt, a .yaml, straight text or a [key,key] => value to the corpus.
+	#If adding something besides [key,key] => value, just pass an argument for key1.
+	#hould be used in most cases.
 	def add(key1,key2="",value="")
 		if !(key2 == "" and value == "") then
-			@corpus.has_key?([key1,key2]) ? @corpus[[key1,key2]] << value : @corpus[[key1,key2]] = [value]
+			add_set key1,key2,value
 		elsif /[\w|\W]*?\.yaml/ =~ key1 then
-			load_yaml key1
+			add_yaml key1
 		elsif /[\w|\W]*?\.txt/ =~ key1 then
-			File.open(key1) do |f| add(f.read) end
+			add_txt key1
 		elsif key1.instance_of? String then
-			set = key1.split
-			set.each_index do |i|
-				if i < set.length - 2 then add(set[i],set[i+1],set[i+2]) end
-			end
+			add_str key1
 		end
 	end
+	
+	#Adds a single set to the corpus- [key, key] => value.
+	def add_set(key1, key2, value)
+		@corpus.has_key?([key1,key2]) ? @corpus[[key1,key2]] << value : @corpus[[key1,key2]] = [value]
+	end
+	
+	#Adds the contents of a text file to the corpus.
+	def add_txt(file)
+		File.open(file) do |f| add_str(f.read) end
+	end
+	
+	#Adds a string to the corpus.
+	def add_str(text)
+		set = text.split
+		set.each_index do |i|
+			if i < set.length - 2 then add_set set[i],set[i+1],set[i+2] end
+		end
+	end 
 		
-	#merges the current corpus with the corpus of the other quoth
+	#Merges the current corpus with the corpus of the other quoth.
 	def merge(oQuoth)
 		return if !oQuoth.is_a?(Quoth)
 		oQuoth.corpus.each_pair do |key, value|
@@ -59,7 +75,7 @@ class Quoth
 		end
 	end
 	
-	#converts the corpus to a string representation for printing
+	#Converts the corpus to a string representation for printing.
 	def to_s
 		ret = ""
 		@corpus.each_pair do |key, value|
@@ -71,7 +87,7 @@ class Quoth
 		ret.chop.chop
 	end
 	
-	#Writes the corpus to yaml- may take a while for large corpuses
+	#Writes the corpus to yaml- may take a while for large corpuses.
 	def to_yaml(fileName="")
 		if fileName != "" then
 			File.open(fileName, 'w') do |f|
@@ -82,8 +98,8 @@ class Quoth
 		end
 	end
 	
-	#loads from yaml into the corpus
-	def load_yaml(fileName)
+	#Loads from yaml into the corpus.
+	def add_yaml(fileName)
 		temp = YAML.load_file fileName
 		temp.each_pair do |key, value|
 			for val in value do
